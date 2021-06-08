85bb2b1a251da40606d320be41e1eb4ecc69e783
Sean Remenyi <sean.remenyi@gmail.com>
2021-06-08 21:40:02 +1000

    game completed

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 Day 25 - US States
 
+A guessing game of American states where you need to name every state
+
+Type states or exit to quit. If you quit a .csv will be generated with all states you were unable to guess.
 
 Highlights: Manipulating .csv with panda
--- main.py
+++ main.py
@@ -9,16 +9,28 @@ turtle.shape(image)
 
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
+guessed_states = []
 
-answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name")
-print(answer_state)
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name").title()
 
-if answer_state in all_states:
-    pointer = turtle.Turtle()
-    pointer.hideturtle()
-    pointer.penup()
-    state_data = data[data.state == answer_state]
-    pointer.goto(int(state_data.x), int(state_data.y))
-    pointer.write(answer_state)
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
 
-screen.exitonclick()
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        pointer = turtle.Turtle()
+        pointer.hideturtle()
+        pointer.penup()
+        state_data = data[data.state == answer_state]
+        pointer.goto(int(state_data.x), int(state_data.y))
+        pointer.write(answer_state)
+
+states_to_learn.csv
